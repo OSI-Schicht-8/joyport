27ce389838ecd1005a683c7085629a0755d2eaf8
OSI-Schicht-8 <77168484+OSI-Schicht-8@users.noreply.github.com>
2021-01-08 19:48:34 +0100

    Update joyport_stubs.asm

--- joyport_stubs.asm
+++ joyport_stubs.asm
@@ -8,11 +8,11 @@
 	xdef	_getjoyport
 
 _getjoyport:
-	move.l		a6,-(sp)			; save register a6
-	move.l		8(sp),d0			; copy param to register
+	move.l		a6,-(sp)		; save register a6
+	move.l		8(sp),d0		; copy param to register
 	move.l		_joyportBase,a6		; library base to a6
-	jsr			_LVOgetjoyport(a6)	; go to joyport routine
-	move.l		(sp)+,a6			; restore register a6
+	jsr		_LVOgetjoyport(a6)	; go to joyport routine
+	move.l		(sp)+,a6		; restore register a6
 	rts
 
 	end
