641891bf006b3fe9ef640bf1f9522c61bb4b1157
OSI-Schicht-8 <77168484+OSI-Schicht-8@users.noreply.github.com>
2021-01-08 19:53:56 +0100

    Add files via upload

---
+++ joyporttest.c
@@ -0,0 +1,24 @@
+#include <proto/exec.h>
+#include <stdio.h>
+
+struct Library *joyportBase = NULL;
+
+int main() {
+	LONG status;
+
+	/* open joyport.library */
+	if(!(joyportBase=OpenLibrary("joyport.library",0))) {
+		printf("Can't open joyport.library\n");
+		exit(20);
+	}
+
+	/* call getjoyport function from joyport.library */
+	status = getjoyport(0);
+	printf("status of joyport 0: %lx\n", status);
+
+	status = getjoyport(1);
+	printf("status of joyport 1: %lx\n", status);
+
+	CloseLibrary(joyportBase);
+	exit(0);
+}
